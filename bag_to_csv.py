80f7e5dae6ad28859b6582c147f02abf52649690
Enri2077 <enry2077@gmail.com>
2025-10-02 05:36:03 +0200

    added ranges data from UWB topics to jupiter notebooks

--- bag_to_csv.py
+++ bag_to_csv.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from collections import defaultdict
 from pathlib import Path
 from os import path
@@ -6,9 +8,9 @@ import pandas as pd
 
 
 def bag_to_csv(bag_path: str) -> None:
-    topic_field_data = defaultdict(list)
 
     with AnyReader([Path(bag_path)]) as reader:
+        topic_field_data = defaultdict(list)
         connections = [x for x in reader.connections if x.topic == "/UWB/Pos"]
         for connection, timestamp, rawdata in reader.messages(connections=connections):
             msg = reader.deserialize(rawdata, connection.msgtype)
@@ -20,9 +22,20 @@ def bag_to_csv(bag_path: str) -> None:
         df = pd.DataFrame(topic_field_data)
         df.to_csv(path.join(bag_path, f"position.csv"))
 
+    with AnyReader([Path(bag_path)]) as reader:
+        topic_field_data = defaultdict(list)
+        connections = [x for x in reader.connections if x.topic == "/UWB/Ranges"]
+        for connection, timestamp, rawdata in reader.messages(connections=connections):
+            msg = reader.deserialize(rawdata, connection.msgtype)
+            topic_field_data['t'].append(timestamp/1E9)
+            topic_field_data['r'].append(msg.range)
+
+        df = pd.DataFrame(topic_field_data)
+        df.to_csv(path.join(bag_path, f"ranges.csv"))
+
 
 def main():
-    bag_to_csv(path.expanduser('~/uwb_logs/laser_tracker/uwb_laser_tracker_bags/rosbag2_2024_07_26-12_14_22'))
+    bag_to_csv(path.expanduser('~/uwb_logs/laser_tracker/uwb_laser_tracker_bags/rosbag2_2024_07_26-12_28_39'))
 
 
 if __name__ == '__main__':
